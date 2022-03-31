b1e63ae4abc9684ee22a758492c578fa3a7d6917
paul88vera <paul88vera@gmail.com>
2022-03-31 18:09:53 -0500

    intial files

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+ return (
+  <footer>
+   <a href="#GitHub">github logo</a>
+   <a href="#LinkedIn">linkedin logo</a>
+   <a href="StackO">stack overflow logo</a>
+  </footer>
+ )
+}
+
+export default Footer;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header>
+      <h1>Paul Vera</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#port">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#resume">Resume</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
