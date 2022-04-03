0319b6753bf092dd0d3d7b7121a62e1f024f4ca2
paul88vera <paul88vera@gmail.com>
2022-04-03 00:10:19 -0500

    footer css completed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,38 @@
 import React from "react";
 
 function Footer() {
- return (
-  <footer>
-   <a href="#GitHub">github logo</a>
-   <a href="#LinkedIn">linkedin logo</a>
-   <a href="StackO">stack overflow logo</a>
-  </footer>
- )
+  return (
+    <footer>
+      <div>
+        <a
+          href="https://github.com/paul88vera"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="./images/icon-github.png" alt="github link" width="40px" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/paul-vera-074ab1a5/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="./images/LI-Logo.png" alt="linked-in link" width="40px" />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/17246597/paul-vera"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="./images/StackOverflow-icon.png"
+            alt="stack overflow link"
+            width="40px"
+          />
+        </a>
+      </div>
+      <p>Paul Vera © Copyright 2022</p>
+    </footer>
+  );
 }
 
 export default Footer;
