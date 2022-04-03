3b98df0ae746578b16b2de2225852ab9d6c4150e
paul88vera <paul88vera@gmail.com>
2022-04-03 00:52:45 -0500

    about css is mobile & desktop ready

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 function About() {
- return (
-  <section>
-   <img src={'./images/pro-pic.jpg'} alt="" />
-  </section>
- )
+  return (
+    <section className="about">
+     <h3>About Me</h3>
+      <div className="proPic">
+        <img src={"./images/pro-pic.png"} alt="" width="200px" />
+      </div>
+      <div className="about-para">
+        <span>The name is Paul Vera.</span> I'm just a dude who wants to code some awesome websites and
+        applications. I've been learning, implementing, and teaching <span>front-end
+        design</span>, and <span>graphic design</span> for the past <span>5 years.</span> I've recently have been diving deeper into <span>back-end
+        coding</span> and have transitioned into a <span>MERN full-stack developer!</span> Take a look at
+        my portfolio and <span>contact me if you need an awesome
+        website!!</span>
+      </div>
+    </section>
+  );
 }
 
 export default About;
