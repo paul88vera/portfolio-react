48cf43e98db07f8a5fae8a7699d75330221fc724
paul88vera <paul88vera@gmail.com>
2022-04-29 10:03:50 -0500

    bio correction

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,8 +10,8 @@ function About() {
       <div className="about-para">
         <span>The name is Paul Vera.</span> I'm just a dude who wants to code some awesome websites and
         applications. I've been learning, implementing, and teaching <span>front-end
-        design</span>, and <span>graphic design</span> for the past <span>5 years.</span> I've recently have been diving deeper into <span>back-end
-        coding</span> and have transitioned into a <span>MERN full-stack developer!</span> Take a look at
+        design</span>, and <span>graphic design</span> for the past <span>5 years.</span> I've been diving deeper into <span>back-end
+        coding</span> and have transitioned into a <span>MERN full-stack developer!</span> I also completed a <span>24 week MERN Full Stack Bootcamp</span> at the <span>University of Texas at Austin</span>. Take a look at
         my portfolio and <span>contact me if you need an awesome
         website!!</span>
       </div>
